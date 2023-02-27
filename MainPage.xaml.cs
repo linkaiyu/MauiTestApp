38bfba0b6422c40db0f42f9ac6175825c0391eb4
linkaiyu <lyu11111@hotmail.com>
2023-02-27 10:30:10 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+
+        string[] resNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+        foreach (string resName in resNames) { Debug.WriteLine(resName); }
+	}
+
+}
+
